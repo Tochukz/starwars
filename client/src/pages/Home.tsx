929c26d576f55b4c5b66cbc2c3ef32fe6b463ba6
Tochukwu Nwachukwu <truetochukz@gmail.com>
2022-06-10 09:39:53 +0200

    Added Page component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,50 +1,7 @@
-import { useEffect } from "react";
-import { connect } from "react-redux";
+import Page from "./Page";
 
-import Base from '../layout/Base';
-import Pagination from "../components/Pagination";
-import Person from "../components/Person";
-import { IPerson } from "../interfaces/iPerson";
-import { getPeople } from '../store/slices/people.slice';
-
-
-function Home(props: {people: IPerson[], queryPeople: Function}) {
-
-  useEffect(() => {
-    getPeople(1);
-  }, []);
-
-  const getPeople = (page: number = 0, name: string = '') => {
-    props.queryPeople(page, name)
-         .then()
-         .catch((err: any) => {
-             console.error('err', err);
-         })
-  }
-
+export default function Home() {
   return (
-    <Base>
-      <div className="home row">
-          <div className="col-sm-12">
-            <Pagination pageMeta={{current: 1}} />
-          </div>
-          { props.people.map((person: IPerson, i: number) => 
-            <Person person={person} key={i} />
-          )}
-          <div className="col-sm-12">
-            <Pagination pageMeta={{current: 1}} />
-          </div>
-      </div>
-    </Base>
+    <Page />
   )
 }
-
-const mapStateToProps = (state: any) => ({
-  people: state.people.people || [],
-});
-
-const mapDispatchToProps = {
-  queryPeople: getPeople,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
